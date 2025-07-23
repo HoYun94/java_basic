7abc42b3ae4e37fe4f39139fde40b23ab1fa306f
YUNHO <y20318@naver.com>
2025-07-23 14:09:04 +0900

    test_01

--- src/ex01.java
+++ src/ex01.java
@@ -11,14 +11,14 @@ public class ex01 {
         sum = num1 + num2 + num3;
 
         System.out.println("10+11+12 = " + sum);
-        System.out.println(num1 + "+" + num2+ "+" num3 "=" + sum);
-        System.out.printf("%d + %d + %d = %d " , num1,num2, num3, sum);
+        System.out.println(num1 + "+" + num2 + "num3  =" + sum);
+        System.out.printf("%d + %d + %d = %d ", num1, num2, num3, sum);
 
-
+    }
 
     }
 
 
 
 
-}
+
